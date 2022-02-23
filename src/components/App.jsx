2c92e0049225103b0741b30d73aa3251faf82050
codajoker <codajoker91@gmail.com>
2022-02-23 23:42:46 +0200

    feat add markup and logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,70 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { ColumnGroupingTable } from './Table/Table';
+import { BasicModal } from './Modal/Modal';
+import { nanoid } from 'nanoid';
+
 export const App = () => {
+  const [arrayClients, setArrayClients] = useState(() => {
+    return (
+      JSON.parse(window.localStorage.getItem('clients')) ?? [
+        {
+          value: 5,
+          data: '20.20.2020',
+          user: 'Vlad',
+          comment: 'vay',
+          id: nanoid(),
+        },
+      ]
+    );
+  });
+  const [objectCity, setObjectCity] = useState(null);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const addContact = data => {
+    setArrayClients(prevState => {
+      return [
+        {
+          value: data.value,
+          data: data.data,
+          user: data.user,
+          comment: data.comment,
+          id: nanoid(),
+        },
+        ...prevState,
+      ];
+    });
+  };
+  async function API() {
+    const fetch = await axios.get(
+      'https://6214d79989fad53b1f210931.mockapi.io/city'
+    );
+    const data = fetch.data[0];
+    return setObjectCity(data);
+  }
+  useEffect(() => {
+    window.localStorage.setItem('clients', JSON.stringify(arrayClients));
+  }, [arrayClients]);
+  useEffect(() => {
+    API();
+  }, []);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {objectCity && (
+        <>
+          <ColumnGroupingTable
+            handleOpen={handleOpen}
+            objectCity={objectCity}
+          ></ColumnGroupingTable>
+          <BasicModal
+            arrayClients={arrayClients}
+            addContact={addContact}
+            open={open}
+            handleClose={handleClose}
+          ></BasicModal>
+        </>
+      )}
+    </>
   );
 };
