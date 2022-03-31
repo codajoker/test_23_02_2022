0f148d8a2c60543d98c77e090a1a061e99b59e78
codajoker <codajoker91@gmail.com>
2022-03-31 16:30:29 +0200

    feat add BrowserRouter basename="/test_23_02_2022/"

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/test_23_02_2022/">
       {' '}
       <App />
     </BrowserRouter>
